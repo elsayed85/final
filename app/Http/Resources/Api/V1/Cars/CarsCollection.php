789878e218a6fb728a7a2d0e005a9d3ff65e30f7
unknown <elsayedkamal581999@gmail.com>
2020-11-12 01:15:10 +0200

    “wip”

--- app/Http/Resources/Api/V1/Cars/CarsCollection.php
+++ app/Http/Resources/Api/V1/Cars/CarsCollection.php
@@ -14,6 +14,6 @@ class CarsCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->collection;
     }
 }
