26da54a9eb99fe22c668925b08e2880ae02503ba
SID <sid@SID-MacBook-Pro.local>
2020-12-15 20:09:39 +0200

    “wip”

--- resources/views/biocodeMail.blade.php
+++ resources/views/biocodeMail.blade.php
@@ -1,14 +1,5 @@
-{{-- <h1>
+<h1>
     Thank you <strong>{{ $el->name }}</strong> for filling out the event form. Get Your Guied.
     Keep the ID that you will receive because attendance will be based on the serial number.♥️ <br>
     <strong>your id is {{ $el->id }}</strong>
 </h1>
- --}}
-
- @php
-    if(isset($message)){
-        var_dump($message);
-    }else{
-        echo "not found";
-    }
- @endphp
