a770aaef81f2983315e4d90d26635f9e0d84a200
SID <sid@SID-MacBook-Pro.local>
2020-12-17 23:57:35 +0200

    “wip”

--- app/Jobs/SendBioCodeUserEmailJob.php
+++ app/Jobs/SendBioCodeUserEmailJob.php
@@ -2,12 +2,14 @@
 
 namespace App\Jobs;
 
+use App\Mail\BioCodeUserMail;
 use App\Models\BioCode\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class SendBioCodeUserEmailJob implements ShouldQueue
 {
