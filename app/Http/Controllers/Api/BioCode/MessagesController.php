f9fc83df25b1dbd566bd998a0a19bc14c7a59ace
SID <sid@SID-MacBook-Pro.local>
2020-12-17 22:47:33 +0200

    “wip”

--- app/Http/Controllers/Api/BioCode/MessagesController.php
+++ app/Http/Controllers/Api/BioCode/MessagesController.php
@@ -13,12 +13,30 @@ class MessagesController extends Controller
 {
     public function send(Request $request)
     {
+        $emailSent = false;
+
         $request->validate([
             'name' => ['required', 'min:3', 'max:40'],
             'email' => ['required', 'email', 'max:60', 'unique:bio_code_users,email'],
             'phone' => ['required', 'min:10', 'max:30', 'unique:bio_code_users,phone'],
             'from_mansoura_university' => ['nullable' , 'boolean'],
         ]);
+
+        $usersFromMansoura = User::where('from_mansoura_university' , true)->count();
+
+        $userNotFromMansoura = User::where('from_mansoura_university' , false)->count();
+
+        if($request->from_mansoura_university && $usersFromMansoura >= 100){
+            return response()->json([
+                'success' => false,
+                'message' => "لقد تم إغلاق الحجز لطلاب جامعه المنصوره لإكتمال العدد"
+            ]);
+        }elseif(!$request->from_mansoura_university && $userNotFromMansoura >= 80){
+            return response()->json([
+                'success' => false,
+                'message' => "لقد تم إغلاق الحجز لمن هم خارج جامعه المنصوره لإكتمال العدد"
+            ]);
+        }
 
         $user = User::create([
             'name' => $request->name,
@@ -29,15 +47,33 @@ class MessagesController extends Controller
 
         try {
             Mail::to($request->email)->send(new BioCodeUserMail($user));
+            $emailSent = true;
         } catch (Exception $e) {
             //
         }
 
 
-        return response()->json([
-            'user_id' => $user->id,
-            'success' =>  true
-        ]);
+        if($emailSent){
+            return response()->json([
+                'success' => true,
+                'message' => "لقد تم إرسال رسالة على بريدك الالكتروني  تحتوي على معلومات التسجيل ورقم التذكره ",
+                'ticket' => [
+                    'id' => $user->id,
+                    'paid' => $user->from_mansoura_university,
+                    'price' => $user->ticket_price
+                ]
+            ]);
+        }else{
+            return response()->json([
+                'success' => true,
+                'message' => "تم التسجيل بنجاح و يرجاء الاحتفاظ برقم التذكره",
+                'ticket' => [
+                    'id' => $user->id,
+                    'paid' => $user->from_mansoura_university,
+                    'price' => $user->ticket_price
+                ]
+            ]);
+        }
     }
 
     public function getAllMessages()
