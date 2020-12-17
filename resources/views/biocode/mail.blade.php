6841b9c53a7c9aeaac71936948edeadbae7faafc
SID <sid@SID-MacBook-Pro.local>
2020-12-17 23:00:17 +0200

    “wip”

--- resources/views/biocode/mail.blade.php
+++ resources/views/biocode/mail.blade.php
@@ -2,21 +2,24 @@
 # Ticket #{{ $user->id }}
 
 @component('mail::table')
-| Name       | Phone     |   Ticket Price   |
+| Name | Phone | Ticket Price |
 | ------------- |:-------------:| --------:|
-| {{ $user->name }}     |   {{ $user->phone }}    |   {{ $user->ticket_price }}
+| {{ $user->name }} | {{ $user->phone }} | {{ $user->ticket_price }}
 @endcomponent
 
 @if(!$user->from_mansoura_university)
 @component('mail::panel')
-Event Location : ITI - Information Technology Institute (Mansoura University) <br>
+Event Location : ITI - Information Technology Institute (Mansoura University)
+<hr>
 <time>
     <span>DEC 23 – DEC 24</span>
     <span>10:00 AM to 04:30 PM</span>
 </time>
 <hr>
-You can Pay via vodafone cash <br>
-phone number : 123456789
+<p>
+    You can Pay via vodafone cash <br>
+    phone number : 123456789
+</p>
 @endcomponent
 @endif
 
